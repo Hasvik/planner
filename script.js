c6c8b5a089a581abb44ec48212e4a4f1e29eb27b
Hasvik <lexroller@gmail.com>
2020-01-20 23:34:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,20 @@ for (let i = 0; i < 2; i++) {
     a != '' && b != '' && a.length < 50) {
         appData.expenses[a] = b;
     } else {
-        
-    }
-    
-}
 
-alert("Ваш бюджет на сегодня: " + appData.budget/30);
+    }
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ваш бюджет на сегодня: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 200) {
+    console.log("Низкий уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка, проверьте введенные данные");
+}
